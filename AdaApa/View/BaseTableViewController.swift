b4bb398cd9da2c7e6eca4c31fa504b178481b807
Dani Anggara <47098245+danianggara@users.noreply.github.com>
2022-08-01 10:47:48 +0700

    update table view cell

--- AdaApa/View/BaseTableViewController.swift
+++ AdaApa/View/BaseTableViewController.swift
@@ -47,7 +47,7 @@ class BaseTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
